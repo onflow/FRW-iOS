839536b047815299e562649fa0482352af1bab7d
cat <zhouxl72@126.com>
2025-04-16 12:39:40 +0800

    feat: on main thread

--- FRW/Modules/NFT/ViewModel/NFTDetailPageViewModel.swift
+++ FRW/Modules/NFT/ViewModel/NFTDetailPageViewModel.swift
@@ -27,15 +27,15 @@ class NFTDetailPageViewModel: ObservableObject {
 
             Task {
                 if let svg = await SVGCache.cache.getSVG(rawSVGURL) {
-                    DispatchQueue.main.async {
+                    await MainActor.run {
                         self.svgString = svg
                     }
                 } else if let svg = nft.imageSVGStr {
-                    DispatchQueue.main.async {
+                    await MainActor.run {
                         self.svgString = svg
                     }
                 } else {
-                    DispatchQueue.main.async {
+                    await MainActor.run {
                         self.nft.isSVG = false
                         self.nft.response.postMedia?.image = self.nft.response.postMedia?.image?
                             .convertedSVGURL()?.absoluteString
