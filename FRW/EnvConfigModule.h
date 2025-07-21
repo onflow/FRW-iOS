cba4bb7f479f538151931bebbb16589c7f64c9e3
Eugene Luzgin <eluzgin@Eugenes-MacBook-Pro.local>
2025-07-21 03:01:25 +0300

    Link native env vars

--- FRW/EnvConfigModule.h
+++ FRW/EnvConfigModule.h
@@ -12,4 +12,19 @@
 + (NSString *)getFlowAccessNodeUrl;
 + (NSString *)getFlowDiscoveryWalletUrl;
 
+// Native App Environment Variables
++ (NSString *)getDriveAesIv;
++ (NSString *)getDriveAesKey;
++ (NSString *)getWalletConnectProjectId;
++ (NSString *)getInstabugTokenDev;
++ (NSString *)getInstabugTokenProd;
++ (NSString *)getCrowdinProjectId;
++ (NSString *)getCrowdinApiToken;
++ (NSString *)getCrowdinDistribution;
++ (NSString *)getMixpanelTokenDev;
++ (NSString *)getMixpanelTokenProd;
++ (NSString *)getDropboxAppKeyDev;
++ (NSString *)getDropboxAppKeyProd;
++ (NSString *)getXSignatureKey;
+
 @end 
--- FRW/EnvConfigModule.m
+++ FRW/EnvConfigModule.m
@@ -41,6 +41,59 @@ RCT_EXPORT_MODULE();
     return [self getEnvVar:@"FLOW_DISCOVERY_WALLET_URL"];
 }
 
+// Native App Environment Variables
++ (NSString *)getDriveAesIv {
+    return [self getEnvVar:@"DRIVE_AES_IV"];
+}
+
++ (NSString *)getDriveAesKey {
+    return [self getEnvVar:@"DRIVE_AES_KEY"];
+}
+
++ (NSString *)getWalletConnectProjectId {
+    return [self getEnvVar:@"WALLET_CONNECT_PROJECT_ID"];
+}
+
++ (NSString *)getInstabugTokenDev {
+    return [self getEnvVar:@"INSTABUG_TOKEN_DEV"];
+}
+
++ (NSString *)getInstabugTokenProd {
+    return [self getEnvVar:@"INSTABUG_TOKEN_PROD"];
+}
+
++ (NSString *)getCrowdinProjectId {
+    return [self getEnvVar:@"CROWDIN_PROJECT_ID"];
+}
+
++ (NSString *)getCrowdinApiToken {
+    return [self getEnvVar:@"CROWDIN_API_TOKEN"];
+}
+
++ (NSString *)getCrowdinDistribution {
+    return [self getEnvVar:@"CROWDIN_DISTRIBUTION"];
+}
+
++ (NSString *)getMixpanelTokenDev {
+    return [self getEnvVar:@"MIXPANEL_TOKEN_DEV"];
+}
+
++ (NSString *)getMixpanelTokenProd {
+    return [self getEnvVar:@"MIXPANEL_TOKEN_PROD"];
+}
+
++ (NSString *)getDropboxAppKeyDev {
+    return [self getEnvVar:@"DROPBOX_APP_KEY_DEV"];
+}
+
++ (NSString *)getDropboxAppKeyProd {
+    return [self getEnvVar:@"DROPBOX_APP_KEY_PROD"];
+}
+
++ (NSString *)getXSignatureKey {
+    return [self getEnvVar:@"X_SIGNATURE_KEY"];
+}
+
 RCT_EXPORT_METHOD(getAllEnvVars:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
     @try {
@@ -53,7 +106,22 @@ RCT_EXPORT_METHOD(getAllEnvVars:(RCTPromiseResolveBlock)resolve
             @"ENVIRONMENT": [self getEnvironment],
             @"FLOW_NETWORK": [self getFlowNetwork],
             @"FLOW_ACCESS_NODE_URL": [self getFlowAccessNodeUrl],
-            @"FLOW_DISCOVERY_WALLET_URL": [self getFlowDiscoveryWalletUrl]
+            @"FLOW_DISCOVERY_WALLET_URL": [self getFlowDiscoveryWalletUrl],
+            
+            // Native App Environment Variables
+            @"DRIVE_AES_IV": [self getDriveAesIv],
+            @"DRIVE_AES_KEY": [self getDriveAesKey],
+            @"WALLET_CONNECT_PROJECT_ID": [self getWalletConnectProjectId],
+            @"INSTABUG_TOKEN_DEV": [self getInstabugTokenDev],
+            @"INSTABUG_TOKEN_PROD": [self getInstabugTokenProd],
+            @"CROWDIN_PROJECT_ID": [self getCrowdinProjectId],
+            @"CROWDIN_API_TOKEN": [self getCrowdinApiToken],
+            @"CROWDIN_DISTRIBUTION": [self getCrowdinDistribution],
+            @"MIXPANEL_TOKEN_DEV": [self getMixpanelTokenDev],
+            @"MIXPANEL_TOKEN_PROD": [self getMixpanelTokenProd],
+            @"DROPBOX_APP_KEY_DEV": [self getDropboxAppKeyDev],
+            @"DROPBOX_APP_KEY_PROD": [self getDropboxAppKeyProd],
+            @"X_SIGNATURE_KEY": [self getXSignatureKey]
         };
         resolve(envVars);
     } @catch (NSException *exception) {
