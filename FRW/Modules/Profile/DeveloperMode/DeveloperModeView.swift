489c8e46675b6984520ba99ee905bae89bac56e0
cat <zhouxl72@126.com>
2025-10-10 21:40:04 +0800

    feat: remove debug

--- FRW/Modules/Profile/DeveloperMode/DeveloperModeView.swift
+++ FRW/Modules/Profile/DeveloperMode/DeveloperModeView.swift
@@ -418,15 +418,15 @@ struct DeveloperModeView: RouteableView {
                                   HUD.success(title: "done.")
                               }
                               //MARK: -
-                              HStack {
-                                Text("Delete SE on Keychain")
-                                Spacer()
-                              }
-                              .frame(height: 64)
-                              .padding(.horizontal, 16)
-                              .onTapGesture {
-                                Router.route(to: RouteMap.Developer.deleteSE)
-                              }
+//                              HStack {
+//                                Text("Delete SE on Keychain")
+//                                Spacer()
+//                              }
+//                              .frame(height: 64)
+//                              .padding(.horizontal, 16)
+//                              .onTapGesture {
+//                                Router.route(to: RouteMap.Developer.deleteSE)
+//                              }
                             }
                             .background(.LL.bgForIcon)
                             .cornerRadius(16)
