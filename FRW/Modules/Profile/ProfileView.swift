5583ba27cd97865e9319619d93d419bf5462b9bb
cat <zhouxl72@126.com>
2025-11-27 13:56:59 +0800

    feat: account list

--- FRW/Modules/Profile/ProfileView.swift
+++ FRW/Modules/Profile/ProfileView.swift
@@ -261,12 +261,7 @@ extension ProfileView {
                     iconName: "icon-wallet",
                     title: "wallets".localized
                 ) {
-#if DEBUG
                   Router.route(to: RouteMap.Profile.accountList)
-#else
-                  Router.route(to: RouteMap.Profile.walletList)
-#endif
-      
                 }
             }
             .padding(.vertical, 20)
