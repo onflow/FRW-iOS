bfd7b9f18616775575ca605552fedf4bea4c9020
lmcmz <lmcmze@gmail.com>
2025-12-01 15:38:49 +1100

    feat: add navigate back logic if redirect native is available

--- FRW/Services/Manager/WalletConnect/WalletConnectManager.swift
+++ FRW/Services/Manager/WalletConnect/WalletConnectManager.swift
@@ -979,6 +979,7 @@ extension WalletConnectManager {
                 )
 
                 HUD.success(title: "approved".localized)
+                self.navigateBackTodApp(for: request.topic)
             } catch {
                 log.error("WalletConnectManager -> approveRequest failed", context: error)
                 rejectRequest(request: request)
@@ -1050,6 +1051,7 @@ extension WalletConnectManager {
                     response: response
                 )
                 HUD.success(title: "rejected".localized)
+                self.navigateBackTodApp(for: request.topic)
             } catch {
                 log.error("WalletConnectManager -> approveRequest failed", context: error)
                 HUD.error(title: "reject_failed".localized)
