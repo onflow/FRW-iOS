7aee0f07d07596e1775c14d4bdaa482a28d72ba7
cat <zhouxl72@126.com>
2025-12-23 13:45:41 +0800

    feat: bar item on iOS 26

--- FRW/Modules/Wallet/TokenDetail/TokenDetailView.swift
+++ FRW/Modules/Wallet/TokenDetail/TokenDetailView.swift
@@ -82,15 +82,18 @@ struct TokenDetailView: RouteableView {
                 MoveTokenView(tokenModel: vm.token, isPresent: $vm.showSheet)
             }
         }
-        .navigationBarItems(trailing: HStack(spacing: 6) {
-            Menu(systemImage: "ellipsis") {
-                Button("Delete EFT", systemImage: "trash") {
-                    vm.deleteCustomToken()
+        .toolbar {
+            if vm.showDeleteToken {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu(systemImage: "ellipsis") {
+                        Button("Delete EFT", systemImage: "trash") {
+                            vm.deleteCustomToken()
+                        }
+                    }
+                    .foregroundStyle(Color.Theme.Background.icon)
                 }
             }
-            .foregroundStyle(Color.Theme.Background.icon)
-            .visibility(vm.showDeleteToken ? .visible : .gone)
-        })
+        }
     }
 
     var summaryView: some View {
